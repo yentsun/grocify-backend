c250e2577dfae8af0d454d10998b3e08ebca3ee3
Maksim Korinets <mkorinets@gmail.com>
2024-11-06 21:22:16 +0700

    Add boot.js to initialize and configure services
    This script loads configuration settings, initializes Kojo and TRID instances, and starts the HTTP server. It ensures all components are ready before proceeding to avoid runtime errors.

---
+++ boot.js
@@ -0,0 +1,29 @@
+import * as path from 'node:path';
+import configLoader from 'yt-config';
+import Kojo from 'kojo';
+import TRID from 'trid';
+
+
+async function boot() {
+
+    console.log('> load config');
+    const config = await configLoader(path.join(process.cwd(), 'config.ini'));
+
+    console.log('> init Kojo');
+    const kojo = new Kojo(config.kojo);
+    kojo.set('config', config);
+    kojo.set('handlers', {});
+
+    console.log('> init trid');
+    kojo.set('trid', new TRID({ prefix: kojo.id, length: 4 }));
+
+    console.log('> wait for ready');
+    await kojo.ready();
+
+    const { HTTP } = kojo.functions;
+    kojo.set('httpServer', await HTTP.listen());
+
+    return kojo;
+}
+
+boot().catch(error => { throw error });
