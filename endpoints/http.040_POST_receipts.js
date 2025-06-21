d89e8605b7add7f3d6bd6726a3680f669ae6eec5
Maksim Korinets <mkorinets@gmail.com>
2025-06-21 10:46:51 +0700

    Refine receipt analysis instructions: update price format to include amount and currency

--- endpoints/http.040_POST_receipts.js
+++ endpoints/http.040_POST_receipts.js
@@ -53,8 +53,7 @@ export default async (app, logger) => {
                     content: [
                         { type: 'text', text: 'Analyze this receipt and return the following information:' +
                                               'json object with receipt id, shop name, location and country and array of data objects ' +
-                                              'with category, sub-category, name, translated name and price. Do not transform the ' +
-                                              'price values to numbers.' },
+                                              'with category, sub-category, name, translated name, price as amount and currency.' },
                         { type: 'image_url', image_url: { url: base64Data }}
                     ]
                 }],
